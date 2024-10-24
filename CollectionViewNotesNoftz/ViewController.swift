166c4d2e215d96bb2eee56980caa3ac3882028bf
Evangeline Noftz <esnoftz@gmail.com>
2024-10-24 07:45:12 -0500

    Finished notes.

--- CollectionViewNotesNoftz/ViewController.swift
+++ CollectionViewNotesNoftz/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
     }
 
-
+    // shows 100 cells
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as UICollectionViewCell
+        
+        // generates random background color for each cell
+        cell.backgroundColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1.0)
+        
+        return cell
+        
+    }
+    
 }
 
